5d696bf9dd12be2e675988a3ee10dcd4ae37eb4c
anuwatavis <anuwat.avis@gmail.com>
2023-05-21 15:49:49 +0700

    feat(): add to_rnt_time to convert datetime to rtn datetime format

--- app/rtnf/src/rtnf.py
+++ app/rtnf/src/rtnf.py
@@ -35,3 +35,23 @@ def to_rnt_time(datetime_to_convert: datetime):
     rnt_time_format = datetime_to_convert.strftime('%H%M')
     return rnt_time_format
 
+
+def to_rtn_datetime(datetime_to_convert: datetime):
+    """
+    Convert the given datetime object to a string in the Royal Thai Navy time format.
+
+    Args:
+        datetime_to_convert: The datetime object to convert.
+
+    Returns:
+        str: The converted time string in the Royal Thai Navy date format.
+
+    Raises:
+        ValueError: If the input is not a valid datetime object.
+    """
+
+    if not is_datetime_object(datetime_to_convert):
+        raise ValueError("Input is not a valid datetime object.")
+
+    rnt_datetime_format = datetime_to_convert.strftime('%d%H%M')
+    return rnt_datetime_format
